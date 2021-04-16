6205dfe8930cf5bd2385ce4dafe660d13916563f
shouyinji1 <shouyinji1@sjldfk.com>
2021-04-16 21:44:17 +0800

    修复数据库连接池不自动关闭的BUG

--- src/dao/impl/BaseDaoImpl.java
+++ src/dao/impl/BaseDaoImpl.java
@@ -1,23 +1,34 @@
 package dao.impl;
 
-import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class BaseDaoImpl<T> {
+public class BaseDaoImpl<T> extends SqlSessionDaoSupport{
 	// 1. 声明SqlSessionFactory
-	@Autowired
-	private SqlSessionFactory sqlSessionFactory;
+	//@Autowired
+	//private SqlSessionFactory sqlSessionFactory;
 	// 2. 声明SqlSession
-	protected SqlSession sqlSession;
+	//@Autowired
+	//protected SqlSession sqlSession;
 	// 3. 声明mapper属性
 	private Class<T> mapper;
 	
+	@Override
+	@Autowired
+	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+		// TODO Auto-generated method stub
+		super.setSqlSessionFactory(sqlSessionFactory);
+	}
+	
 	// 4. 为mapper创建get set
+	//@Autowired
 	public T getMapper() {
-			return sqlSessionFactory.openSession().getMapper(mapper);
+		return getSqlSession().getMapper(mapper);
+		//return sqlSessionFactory.openSession().getMapper(mapper);
+		//return sqlSession.getMapper(mapper);
 	}
 	public void setMapper(Class<T> mapper) {
 		this.mapper=mapper;
